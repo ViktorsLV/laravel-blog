90ee3a06b4648c3f51a82379bb57af9fbf1dcaf9
Viktor <viktors.ch11@gmail.com>
2021-12-05 15:35:34 +0100

    navbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <title>Laravel Blog</title>
+</head>
+<body class="bg-gray-200">
+  <nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+      <li>
+        <a href="/" class="p-3">Home</a>
+      </li>
+      <li>
+        <a href="{{route('posts')}}" class="p-3">Posts</a>
+      </li>
+      <li>
+        <a href="{{route('tags')}}" class="p-3">Tags</a>
+      </li>
+    </ul>
+
+    <ul class="flex items-center">
+      <li>
+        <a href="" class="p-3"> USERNAME </a>
+      </li>
+      <li>
+        {{-- <form action="{{route('logout')}}" method="post" class="inline p-3"> --}}
+          <button type="submit" class="">Logout</button>
+        {{-- </form> --}}
+      </li>
+
+      <li>
+        <a href="{{route('login')}}" class="p-3">Login</a>
+      </li>
+      <li>
+        <a href="{{route('register')}}" class="p-3">Register</a>
+      </li>
+            
+    </ul>
+  </nav>
+
+  @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +19,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register'); // can assign a name so we can look for this route in links href="{{route('register')}}
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+
+Route::get('/tags', [TagController::class, 'index'])->name('tags');
