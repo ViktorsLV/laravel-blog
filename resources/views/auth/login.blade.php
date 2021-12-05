bae57847790558dc7755827eba829df83d7a8a27
Viktor <viktors.ch11@gmail.com>
2021-12-05 23:50:01 +0100

    styling of auth components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,50 +2,65 @@
 
 @section('content')
   <div class="flex justify-center">
-    <div class="w-4/12 bg-purple-100 p-6 rounded-md">
-      @if (session('status')) 
-        <div class="bg-red-400 rounded-md mb-6 p-3 text-white text-center">
-          {{ session('status') }}
+    <div class="w-8/12 bg-white-100 rounded-md flex felx-row border-4 border-purple-100">
+
+      <div class="w-6/12 bg-white-100 p-6 mt-0">
+        <h1 class="text-3xl mb-2 font-bold text-gray-600">Blog Name</h1>
+        <div class="bg-white p-6 rounded-md text-center mx-auto mt-10 text-gray-600">
+          <img src="{{ asset('images/auth.svg') }}" alt="" width="250" class="mx-auto"> 
+          <h1 class="text-2xl mb-2 font-bold mt-4">Login securely</h1>
+          <p>Log in and discover new blog posts</p>
         </div>
-      @endif
+      </div>
+    
+      <div class="w-6/12 bg-purple-100 p-6">
 
-      <form action="{{route('login')}}" method="post">
-        @csrf
-        <div class="mb-4">
-          <label for="email" class="sr-only">Email</label>
-          <input type="text" name="email" id="email" class="bg-white border-2 w-full p-4 rounded-lg @error('email') border-red-400 @enderror" value="{{ old('email')}}" placeholder="Email">
-
-          @error('email')
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror
-        </div>
-
-        <div class="mb-4">
-          <label for="password" class="sr-only">Password</label>
-          <input type="password" name="password" id="password" class="bg-white border-2 w-full p-4 rounded-lg @error('password') border-red-400 @enderror" value="" placeholder="Password">
-
-          @error('password')
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror
-        </div>
-
-        {{-- for remembering user when logging in --}}
-        {{-- TODO: actually remember the user --}}
-        <div class="mb-4">
-          <div class="flex items-center">
-            <input type="checkbox" name="remember" id="remember" class="mr-2">
-            <label for="remember">Remember me</label>
+          @if (session('status')) 
+          <div class="bg-red-400 rounded-md mb-6 p-3 text-white text-center">
+            {{ session('status') }}
           </div>
-        </div>
+          @endif
+          
+          <form action="{{route('login')}}" method="post" class="mt-10">
+            @csrf
+            <div class="mb-4">
+              <label for="email" class="sr-only">Email</label>
+              <input type="text" name="email" id="email" class="bg-white border-2 w-full p-4 rounded-lg @error('email') border-red-400 @enderror" value="{{ old('email')}}" placeholder="Email">
 
-        {{-- TODO: add register link --}}
+              @error('email')
+                <div class="text-red-400 mt-4 text-sm">
+                    {{$message}}
+                </div>
+              @enderror
+            </div>
 
-        <button type="submit" class="bg-purple-400 text-white px-4 py-3 rounded font-medium w-full hover:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Login</button>
-      </form>
+            <div class="mb-4">
+              <label for="password" class="sr-only">Password</label>
+              <input type="password" name="password" id="password" class="bg-white border-2 w-full p-4 rounded-lg @error('password') border-red-400 @enderror" value="" placeholder="Password">
+
+              @error('password')
+                <div class="text-red-400 mt-4 text-sm">
+                    {{$message}}
+                </div>
+              @enderror
+            </div>
+
+            {{-- for remembering user when logging in --}}
+            {{-- TODO: actually remember the user --}}
+            <div class="mb-4">
+              <div class="flex items-center">
+                <input type="checkbox" name="remember" id="remember" class="mr-2">
+                <label for="remember">Remember me</label>
+              </div>
+            </div>
+
+            {{-- TODO: add register link --}}
+
+            <button type="submit" class="bg-purple-500 text-white px-4 py-3 rounded font-medium w-full hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Login</button>
+            <p class="text-center mt-2">Don't have an account? <a class="text-blue-500 hover:text-blue-700" href="{{route('register')}}">Register here</a> </p>
+          </form>
+      </div>
+
     </div>
   </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,69 +2,80 @@
 
 @section('content')
   <div class="flex justify-center">
-    <div class="w-4/12 bg-purple-100 p-6 rounded-md">
-      {{-- TODO: add heading --}}
+    <div class="w-8/12 bg-white-100 rounded-md flex felx-row border-4 border-purple-100">
 
-      <form action="{{route('register')}}" method="post">
-        @csrf {{-- Cross-site request forgerie protector - CSRF token allows middleware to validate the request (creates a hidden input with a value?) --}} 
-        <div class="mb-4">
-          <label for="name" class="sr-only">Name</label>
-          {{-- old('') - helps with reading old session data and if on form submit some values are incorrect the form doesn't go completely empty but saves the old values - ( https://laravel.com/docs/8.x/requests#retrieving-old-input )--}}
-          <input type="text" name="name" id="name" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('name') border-red-400 @enderror" value="{{ old('name')}}" placeholder="Your name">
-
-          @error('name') {{--( https://laravel.com/docs/8.x/blade#validation-errors ) - link to error handling on frontend --}}
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror
+      <div class="w-6/12 bg-white-100 p-6 mt-0">
+        <h1 class="text-3xl mb-2 font-bold text-gray-600">Blog Name</h1>
+        <div class="bg-white p-6 rounded-md text-center mx-auto mt-10 text-gray-600">
+          <img src="{{ asset('images/auth.svg') }}" alt="" width="250" class="mx-auto"> 
+          <h1 class="text-2xl mb-2 font-bold mt-4">Create new profile</h1>
+          <p>Create a new profile and start creating new posts</p>
         </div>
+      </div>
 
-        <div class="mb-4">
-          <label for="username" class="sr-only">Username</label>
-          <input type="text" name="username" id="username" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('username') border-red-400 @enderror" value="{{ old('username')}}" placeholder="Username">
-
-          @error('username')
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror
-        </div>
-
-        <div class="mb-4">
-          <label for="email" class="sr-only">Email</label>
-          <input type="text" name="email" id="email" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('email') border-red-400 @enderror" value="{{ old('email')}}" placeholder="Email">
-
-          @error('email')
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror
-        </div>
-
-        <div class="mb-4">
-          <label for="password" class="sr-only">Password</label>
-          <input type="password" name="password" id="password" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('password') border-red-400 @enderror" value="" placeholder="Password">
-
-          @error('password')
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror
-        </div>
-
-        <div class="mb-4">
-          <label for="password_confirmation" class="sr-only">Password again</label>
-          <input type="password" name="password_confirmation" id="password_confirmation" class="bg-white-100 border-2 w-full p-4 rounded-lg" value="" placeholder="Repeat your password">
-
-          {{-- @error('password_confirmation')
-            <div class="text-red-400 mt-4 text-sm">
-                {{$message}}
-            </div>
-          @enderror --}}
-        </div>
-
-        <button type="submit" class="bg-purple-400 text-white px-4 py-3 rounded font-medium w-full hover:bg-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Register</button>
-      </form>
+      <div class="w-6/12 bg-purple-100 p-6">
+        <form action="{{route('register')}}" method="post">
+          @csrf {{-- Cross-site request forgerie protector - CSRF token allows middleware to validate the request (creates a hidden input with a value?) --}} 
+          <div class="mb-4">
+            <label for="name" class="sr-only">Name</label>
+            {{-- old('') - helps with reading old session data and if on form submit some values are incorrect the form doesn't go completely empty but saves the old values - ( https://laravel.com/docs/8.x/requests#retrieving-old-input )--}}
+            <input type="text" name="name" id="name" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('name') border-red-400 @enderror" value="{{ old('name')}}" placeholder="Your name">
+  
+            @error('name') {{--( https://laravel.com/docs/8.x/blade#validation-errors ) - link to error handling on frontend --}}
+              <div class="text-red-400 mt-4 text-sm">
+                  {{$message}}
+              </div>
+            @enderror
+          </div>
+  
+          <div class="mb-4">
+            <label for="username" class="sr-only">Username</label>
+            <input type="text" name="username" id="username" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('username') border-red-400 @enderror" value="{{ old('username')}}" placeholder="Username">
+  
+            @error('username')
+              <div class="text-red-400 mt-4 text-sm">
+                  {{$message}}
+              </div>
+            @enderror
+          </div>
+  
+          <div class="mb-4">
+            <label for="email" class="sr-only">Email</label>
+            <input type="text" name="email" id="email" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('email') border-red-400 @enderror" value="{{ old('email')}}" placeholder="Email">
+  
+            @error('email')
+              <div class="text-red-400 mt-4 text-sm">
+                  {{$message}}
+              </div>
+            @enderror
+          </div>
+  
+          <div class="mb-4">
+            <label for="password" class="sr-only">Password</label>
+            <input type="password" name="password" id="password" class="bg-white-100 border-2 w-full p-4 rounded-lg @error('password') border-red-400 @enderror" value="" placeholder="Password">
+  
+            @error('password')
+              <div class="text-red-400 mt-4 text-sm">
+                  {{$message}}
+              </div>
+            @enderror
+          </div>
+  
+          <div class="mb-4">
+            <label for="password_confirmation" class="sr-only">Password again</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" class="bg-white-100 border-2 w-full p-4 rounded-lg" value="" placeholder="Repeat your password">
+  
+            {{-- @error('password_confirmation')
+              <div class="text-red-400 mt-4 text-sm">
+                  {{$message}}
+              </div>
+            @enderror --}}
+          </div>
+  
+          <button type="submit" class="bg-purple-500 text-white px-4 py-3 rounded font-medium w-full hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50">Register</button>
+          <p class="text-center mt-2">Already have an account? <a class="text-blue-500 hover:text-blue-700" href="{{route('login')}}">Log in here</a> </p>
+        </form>
+      </div>
 
       {{-- TODO: add "log in button" --}}
     </div>
