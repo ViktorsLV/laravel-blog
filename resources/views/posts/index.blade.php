fd949ec9af1b43f223c326f619adca3e9886602d
Viktor <viktors.ch11@gmail.com>
2021-12-07 17:15:18 +0100

    Changed post form - added title area

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,11 +17,23 @@
       <form action="{{route('posts')}}" method="post">
         <div class="text-2xl font-medium mb-5 text-gray-600">Let the people know what's on your mind:</div>
         @csrf
+        <div class="mb-2">
+          <label for="title" class="sr-only">Title</label>
+          <textarea type="text" name="title" id="title" cols="30" rows="1"
+            class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('title') border-red-500 @enderror"
+            value="{{ old('title')}}" placeholder="Post Tite"></textarea>
+
+          @error('title')
+          <div class="text-red-500 mt-4 text-sm">
+            {{$message}}
+          </div>
+          @enderror
+        </div>
         <div class="mb-4">
           <label for="body" class="sr-only">Body</label>
           <textarea type="text" name="body" id="body" cols="30" rows="4"
             class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
-            value="{{ old('body')}}" placeholder="Post something!"></textarea>
+            value="{{ old('body')}}" placeholder="Type here..."></textarea>
 
           @error('body')
           <div class="text-red-500 mt-4 text-sm">
